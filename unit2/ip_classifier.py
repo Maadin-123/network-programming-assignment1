38090e41a1c3f79d5547f0afed96a59fd2858307
barbs-mn <barbranjerim@gmail.com>
2026-06-18 11:20:58 +0300

    Update ip_classifier.py

--- unit2/ip_classifier.py
+++ unit2/ip_classifier.py
@@ -7,7 +7,4 @@ def classify_ip(ip_string):
     except ValueError:
         return f"{ip_string} is an INVALID IP address."
 
-# Test Cases
-print(classify_ip("192.168.1.1"))
-print(classify_ip("2001:db8::1"))
-print(classify_ip("999.999.999.999"))
+
