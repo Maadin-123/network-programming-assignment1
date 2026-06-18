f0c03dbd965049be6f7880cc26778acde954cfde
barbs-mn <barbranjerim@gmail.com>
2026-06-18 11:23:09 +0300

    Update resolve.py

--- unit2/resolve.py
+++ unit2/resolve.py
@@ -12,8 +12,4 @@ def resolve_ip(ip_address):
     except (socket.herror, socket.gaierror):
         return "Error: Unable to resolve IP address."
 
-# Test Cases
-print("--- Q1: DNS Resolver Results ---")
-print("Forward DNS (google.com):", resolve_hostname("google.com"))
-print("Reverse DNS (8.8.8.8):", resolve_ip("8.8.8.8"))
-print("Invalid Test (fake.domain):", resolve_hostname("fake.domain.invalid"))
+
